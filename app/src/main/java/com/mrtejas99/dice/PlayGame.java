d228867465ea63025667af450f1e4384e26d7d5f
Tejas Lotlikar <mrt3j45@gmail.com>
2020-03-05 20:51:14 +0530

    Orientation locked, UI improvements

--- app/src/main/java/com/mrtejas99/dice/PlayGame.java
+++ app/src/main/java/com/mrtejas99/dice/PlayGame.java
@@ -19,7 +19,7 @@ import java.util.Random;
 public class PlayGame extends AppCompatActivity {
     String moves = "", player1, player2;
     Integer player = 1, count, score1 = 0, score2 = 0;
-    TextView txt_turn, txt_greet;
+    TextView txt_turn, txt_greet, txt_prompt;
     TextView txt_player1, txt_player2;
     Boolean singleMode;
     RotateAnimation animate;
@@ -30,9 +30,12 @@ public class PlayGame extends AppCompatActivity {
         setContentView(R.layout.activity_play_game);
         txt_turn = findViewById(R.id.txt_turn);
         final Integer[] imageIds = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five, R.drawable.six};
+        final Integer[] blkDice = {R.drawable.blkone, R.drawable.blktwo, R.drawable.blkthree, R.drawable.blkfour, R.drawable.blkfive, R.drawable.blksix};
         txt_player1 = findViewById(R.id.txt_player1);
         txt_player2 = findViewById(R.id.txt_player2);
         txt_greet = findViewById(R.id.txt_greet);
+        txt_prompt = findViewById(R.id.txt_prompt);
+        txt_prompt.setVisibility(View.INVISIBLE);
         Button btn_reset = findViewById(R.id.btn_reset);
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,27 +66,30 @@ public class PlayGame extends AppCompatActivity {
                         txt_player1.setText(player1 + " : " + score1.toString());
                         txt_turn.setText(player2 + "'s turn");
                         if (singleMode) {
+                            txt_prompt.setVisibility(View.VISIBLE);
+                            txt_prompt.setText("Please wait...");
                             Handler handler = new Handler();
                             handler.postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
                                     player = 1;
                                     gen = new Random();
-                                    Integer tmp_score2 = gen.nextInt(imageIds.length) + 1;
-                                    Integer randomImg = imageIds[tmp_score2 - 1];
+                                    Integer tmp_score2 = gen.nextInt(blkDice.length) + 1;
+                                    Integer randomImg = blkDice[tmp_score2 - 1];
                                     dice.startAnimation(animate);
                                     dice.setImageResource(randomImg);
                                     score2 += tmp_score2;
                                     txt_player2.setText(player2 + " : " + score2.toString());
                                     txt_turn.setText(player1 + "'s turn");
+                                    txt_prompt.setVisibility(View.INVISIBLE);
                                 }
-                            }, 2000);
+                            }, 1500);
                         }
                     } else {
                         player = 1;
                         Random gen = new Random();
-                        Integer tmp_score2 = gen.nextInt(imageIds.length) + 1;
-                        Integer randomImg = imageIds[tmp_score2 - 1];
+                        Integer tmp_score2 = gen.nextInt(blkDice.length) + 1;
+                        Integer randomImg = blkDice[tmp_score2 - 1];
                         dice.setImageResource(randomImg);
                         score2 += tmp_score2;
                         txt_player2.setText(player2 + " : " + score2.toString());
